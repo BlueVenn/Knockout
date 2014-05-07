8b72c3bd474c6cee88d4682aff47b3fa2c4c1563
bluevenn <kevin.murray@bluegroupinc.com>
2014-05-07 16:24:59 +0100

    Added deployment script

---
+++ Deploy Site.ps1
@@ -0,0 +1,56 @@
+﻿$logFile = "C:\Knockout build.log"
+if(Test-Path -Path $logFile)
+{
+	Remove-Item -Path $logFile
+	New-Item -ItemType file -Path $logFile
+}
+
+$workDir = $args[0]
+Add-Content -Path $logFile -Value "Working Dir = $workDir"
+if (!(Test-Path -Path $workDir))
+{
+	Add-Content -Path $logFile -Value "Working Dir not found"
+	exit
+}
+
+$scriptsDir = [System.IO.Path]::Combine($workDir, "Scripts")
+Add-Content -Path $logFile -Value "Scripts Dir = $scriptsDir"
+if (!(Test-Path -Path $scriptsDir))
+{
+	Add-Content -Path $logFile -Value "Working Dir not found"
+	exit
+}
+
+$templatesDir = [System.IO.Path]::Combine($workDir, "Templates")
+Add-Content -Path $logFile -Value "Templates Dir = $templatesDir"
+if (!(Test-Path -Path $templatesDir))
+{
+	Add-Content -Path $logFile -Value "Working Dir not found"
+	exit
+}
+
+$knockFile = [System.IO.Path]::Combine($workDir, "knockout.html")
+Add-Content -Path $logFile -Value "knockout file = $knockFile"
+if (!(Test-Path -Path $knockFile))
+{
+	Add-Content -Path $logFile -Value "Working Dir not found"
+	exit
+}
+
+$targetDir = "\\JB-2012R2-TEST\C$"
+$targetDir = [System.IO.Path]::Combine($targetDir, "Dev")
+$targetDir = [System.IO.Path]::Combine($targetDir, "Knockout")
+if (!(Test-Path -Path $targetDir))
+{
+	Add-Content -Path $logFile -Value "Target Dir not found"
+	exit
+}
+else
+{
+	Add-Content -Path $logFile -Value "Copying files"
+	Copy-Item -Path $scriptsDir -Recurse -Destination $targetDir -Force
+	Copy-Item -Path $templatesDir -Recurse -Destination $targetDir -Force
+	Copy-Item -Path $knockFile -Destination $targetDir -Force
+	Add-Content -Path $logFile -Value "Files copied"
+}
+
